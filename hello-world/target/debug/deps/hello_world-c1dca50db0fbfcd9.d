29e0e520fe6a292106afd34ef16716d9dd094bd0
Seascouter <seascouterdev@icloud.com>
2022-08-26 09:23:20 -0400

    Completed Hello World Exercise

---
+++ hello-world/target/debug/deps/hello_world-c1dca50db0fbfcd9.d
@@ -0,0 +1,5 @@
+/Users/willgriffin/Desktop/Code/rust-exercism/hello-world/target/debug/deps/hello_world-c1dca50db0fbfcd9: src/main.rs
+
+/Users/willgriffin/Desktop/Code/rust-exercism/hello-world/target/debug/deps/hello_world-c1dca50db0fbfcd9.d: src/main.rs
+
+src/main.rs:
---
+++ hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+/Users/willgriffin/Desktop/Code/rust-exercism/hello-world/target/debug/hello-world: /Users/willgriffin/Desktop/Code/rust-exercism/hello-world/src/main.rs
