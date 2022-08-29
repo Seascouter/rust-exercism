0d7f23b98fc35de156ec0060c90a4ce3ad521eab
Seascouter <seascouterdev@icloud.com>
2022-08-28 22:42:00 -0400

    Completed Health Statistics exercise

---
+++ health-statistics/target/debug/deps/health_statistics-5a6a99a3485a032a.d
@@ -0,0 +1,5 @@
+/Users/willgriffin/Desktop/Code/rust-exercism/health-statistics/target/debug/deps/health_statistics-5a6a99a3485a032a: src/main.rs
+
+/Users/willgriffin/Desktop/Code/rust-exercism/health-statistics/target/debug/deps/health_statistics-5a6a99a3485a032a.d: src/main.rs
+
+src/main.rs:
