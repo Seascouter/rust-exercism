69c7ae6981600469c205778e50e397c929ee8ea0
Seascouter <seascouterdev@icloud.com>
2022-08-31 09:39:20 -0400

    Completed Low-Power Embedded Game exercise

---
+++ low-power-embedded-game/target/debug/deps/low_power_embedded_game-9c63c1f385435153.d
@@ -0,0 +1,5 @@
+/Users/willgriffin/Desktop/Code/rust-exercism/low-power-embedded-game/target/debug/deps/low_power_embedded_game-9c63c1f385435153: src/main.rs
+
+/Users/willgriffin/Desktop/Code/rust-exercism/low-power-embedded-game/target/debug/deps/low_power_embedded_game-9c63c1f385435153.d: src/main.rs
+
+src/main.rs:
---
+++ low-power-embedded-game/target/debug/deps/low_power_embedded_game-d5dd677b44c7daee.d
@@ -0,0 +1,5 @@
+/Users/willgriffin/Desktop/Code/rust-exercism/low-power-embedded-game/target/debug/deps/low_power_embedded_game-d5dd677b44c7daee.rmeta: src/main.rs
+
+/Users/willgriffin/Desktop/Code/rust-exercism/low-power-embedded-game/target/debug/deps/low_power_embedded_game-d5dd677b44c7daee.d: src/main.rs
+
+src/main.rs:
